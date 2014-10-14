f9de34f95b92fb900bc91c79e73747b9b3e17020
Thomas H Jones II <thomas.jones2@dodiis.mil>
2014-10-13 21:40:59 -0400

    Hanlder for STIG ID V-38444 (ip6tables INPUT default 'drop')

---
+++ srv/salt/STIGbyID/cat2/V38444.sls
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# STIG URL: http://www.stigviewer.com/stig/red_hat_enterprise_linux_6/2014-06-11/finding/V-38444
+# Finding ID:	V-38444
+# Version:	RHEL-06-000523
+# Finding Level:	Medium
+#
+#     The systems local IPv6 firewall must implement a deny-all, 
+#     allow-by-exception policy for inbound packets. In "ip6tables" the 
+#     default policy is applied only after all the applicable rules in the 
+#     table are examined for a match. Setting the default policy to "DROP" 
+#     implements proper design for a firewall, ...
+#
+############################################################
+
+script_V38444-describe:
+   cmd.script:
+   - source: salt://STIGbyID/cat2/files/V38444.sh
+
+cmd_V38444:
+  iptables.set_policy:
+  - table: filter
+  - chain: INPUT
+  - policy: DROP
+  - family: ipv6
---
+++ srv/salt/STIGbyID/cat2/files/V38444.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# STIG URL: http://www.stigviewer.com/stig/red_hat_enterprise_linux_6/2014-06-11/finding/V-38444
+# Finding ID:	V-38444
+# Version:	RHEL-06-000523
+# Finding Level:	Medium
+#
+#     The systems local IPv6 firewall must implement a deny-all, 
+#     allow-by-exception policy for inbound packets. In "ip6tables" the 
+#     default policy is applied only after all the applicable rules in the 
+#     table are examined for a match. Setting the default policy to "DROP" 
+#     implements proper design for a firewall, ...
+#
+############################################################
+
+# Standard outputter function
+diag_out() {
+   echo "${1}"
+}
+
+diag_out "----------------------------------"
+diag_out "STIG Finding ID: V-38444"
+diag_out "  Default ip6tables input-policy"
+diag_out "  is drop"
+diag_out "----------------------------------"
