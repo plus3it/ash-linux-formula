e5238eca563136f4f45c08f33ff55b9fae9b357f
Thomas H Jones II <thomas.jones2@dodiis.mil>
2014-10-16 13:13:40 -0400

    Handler for STIG-ID V-38489 (AIDE package must be installed)

---
+++ srv/salt/STIGbyID/cat2/files/V38489.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# STIG URL: http://www.stigviewer.com/stig/red_hat_enterprise_linux_6/2014-06-11/finding/V-38489
+# Finding ID:	V-38489
+# Version:	RHEL-06-000016
+# Finding Level:	Medium
+#
+#     A file integrity tool must be installed. The AIDE package must be 
+#     installed if it is to be available for integrity checking.
+#
+############################################################
+
+# Standard outputter function
+diag_out() {
+   echo "${1}"
+}
+
+diag_out "----------------------------------"
+diag_out "STIG Finding ID: V-38489"
+diag_out "  The AIDE package must be present"
+diag_out "----------------------------------"
