a35c2d4ab753586ac32febc01bbfcd67e0fc7113
Thomas H Jones II <thomas.jones2@dodiis.mil>
2014-10-15 20:46:22 -0400

    Handler for STIG V-38470 (set audit partition space warning-action to 'email')

---
+++ srv/salt/STIGbyID/cat2/V38470.sls
@@ -0,0 +1,22 @@
+# STIG URL: http://www.stigviewer.com/stig/red_hat_enterprise_linux_6/2014-06-11/finding/V-38470
+# Finding ID:	V-38470
+# Version:	RHEL-06-000005
+# Finding Level:	Medium
+#
+#     The audit system must alert designated staff members when the audit 
+#     storage volume approaches capacity. Notifying administrators of an 
+#     impending disk space problem may allow them to take corrective action 
+#     prior to any disruption.
+#
+############################################################
+
+script_V38470-describe:
+  cmd.script:
+  - source: salt://STIGbyID/cat2/files/V38470.sh
+
+file_V38470:
+  file.replace:
+  - name: /etc/audit/auditd.conf
+  - pattern: "^space_left_action =.*"
+  - repl: "space_left_action = email"
+
---
+++ srv/salt/STIGbyID/cat2/files/V38470.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# STIG URL: http://www.stigviewer.com/stig/red_hat_enterprise_linux_6/2014-06-11/finding/V-38470
+# Finding ID:	V-38470
+# Version:	RHEL-06-000005
+# Finding Level:	Medium
+#
+#     The audit system must alert designated staff members when the audit 
+#     storage volume approaches capacity. Notifying administrators of an 
+#     impending disk space problem may allow them to take corrective action 
+#     prior to any disruption.
+#
+############################################################
+
+# Standard outputter function
+diag_out() {
+   echo "${1}"
+}
+
+diag_out "----------------------------------"
+diag_out "STIG Finding ID: V-38470"
+diag_out "  Configure e-mail warning when"
+diag_out "  audit-partition begins to become"
+diag_out "  too full"
+diag_out "----------------------------------"
+
