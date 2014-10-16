5ad98e814590acc188703bfbbfcb0d752ee11c0f
Thomas H Jones II <thomas.jones2@dodiis.mil>
2014-10-16 12:19:40 -0400

    Handler for STIG V-38483 (Enable GPG-check for RPM installs)

---
+++ srv/salt/STIGbyID/cat2/V38483.sls
@@ -0,0 +1,23 @@
+# STIG URL: http://www.stigviewer.com/stig/red_hat_enterprise_linux_6/2014-06-11/finding/V-38483
+# Finding ID:	V-38483
+# Version:	RHEL-06-000013
+# Finding Level:	Medium
+#
+#     The system package management tool must cryptographically verify the 
+#     authenticity of system software packages during installation. 
+#     Ensuring the validity of packages' cryptographic signatures prior to 
+#     installation ensures the provenance of the software and protects 
+#     against malicious tampering.
+#
+############################################################
+
+script_V38483-describe:
+  cmd.script:
+  - source: salt://STIGbyID/cat2/files/V38483.sh
+
+file_V38483:
+  file.replace:
+  - name: /etc/yum.conf
+  - pattern: "^gpgcheck=.*$"
+  - repl: "gpgcheck=1"
+
---
+++ srv/salt/STIGbyID/cat2/files/V38483.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# STIG URL: http://www.stigviewer.com/stig/red_hat_enterprise_linux_6/2014-06-11/finding/V-38483
+# Finding ID:	V-38483
+# Version:	RHEL-06-000013
+# Finding Level:	Medium
+#
+#     The system package management tool must cryptographically verify the 
+#     authenticity of system software packages during installation. 
+#     Ensuring the validity of packages' cryptographic signatures prior to 
+#     installation ensures the provenance of the software and protects 
+#     against malicious tampering.
+#
+############################################################
+
+# Standard outputter function
+diag_out() {
+   echo "${1}"
+}
+
+diag_out "----------------------------------"
+diag_out "  Ensure that GPG-checking is"
+diag_out "  enabled in all yum configuration"
+diag_out "  files (esp. /etc/yum.conf and)"
+diag_out "----------------------------------"
