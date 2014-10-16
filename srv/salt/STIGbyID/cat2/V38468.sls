52c86eb189b200726b420d86eba09af3eea180e3
Thomas H Jones II <thomas.jones2@dodiis.mil>
2014-10-15 20:30:27 -0400

    Handler for STIG V-38468 (set auditd disk_full_action)

---
+++ srv/salt/STIGbyID/cat2/V38468.sls
@@ -0,0 +1,21 @@
+# STIG URL: http://www.stigviewer.com/stig/red_hat_enterprise_linux_6/2014-06-11/finding/V-38468
+# Finding ID:	V-38468
+# Version:	RHEL-06-000510
+# Finding Level:	Medium
+#
+#     The audit system must take appropriate action when the audit storage 
+#     volume is full. Taking appropriate action in case of a filled audit 
+#     storage volume will minimize the possibility of losing audit records.
+#
+############################################################
+
+script_V38468-describe:
+  cmd.script:
+  - source: salt://STIGbyID/cat2/files/V38468.sh
+
+file_V38468:
+  file.replace:
+  - name: /etc/audit/auditd.conf
+  - pattern: "^disk_full_action =.*"
+  - repl: "disk_full_action = HALT"
+
---
+++ srv/salt/STIGbyID/cat2/files/V38468.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# STIG URL: http://www.stigviewer.com/stig/red_hat_enterprise_linux_6/2014-06-11/finding/V-38468
+# Finding ID:	V-38468
+# Version:	RHEL-06-000510
+# Finding Level:	Medium
+#
+#     The audit system must take appropriate action when the audit storage 
+#     volume is full. Taking appropriate action in case of a filled audit 
+#     storage volume will minimize the possibility of losing audit records.
+#
+############################################################
+
+# Standard outputter function
+diag_out() {
+   echo "${1}"
+}
+
+diag_out "----------------------------------"
+diag_out "STIG Finding ID: V-38468"
+diag_out "  Set audit security subsystem's"
+diag_out "  disk_full parameter to 'HALT'"
+diag_out "----------------------------------"
