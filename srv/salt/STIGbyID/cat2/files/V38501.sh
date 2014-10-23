d2ac4de18d3c840217902a559f6ec7223511ebe4
Thomas H Jones II <thomas.jones2@dodiis.mil>
2014-10-23 14:50:55 -0400

    Handler for STIG-ID V-38501

--- srv/salt/STIGbyID/cat2/files/V38501.sh
+++ srv/salt/STIGbyID/cat2/files/V38501.sh
@@ -2,7 +2,7 @@
 #
 # STIG URL: http://www.stigviewer.com/stig/red_hat_enterprise_linux_6/2014-06-11/finding/V-38501
 # Finding ID:	V-38501
-# Version:	
+# Version:	RHEL-06-000357
 # Finding Level:	Medium
 #
 #     The system must disable accounts after excessive login failures 
