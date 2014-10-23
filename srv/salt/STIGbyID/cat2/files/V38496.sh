5565adae23e4bd470073d4b5d3d2d48f767faaac
Thomas H Jones II <thomas.jones2@dodiis.mil>
2014-10-23 14:50:55 -0400

    Handler for STIG-ID V-38496

--- srv/salt/STIGbyID/cat2/files/V38496.sh
+++ srv/salt/STIGbyID/cat2/files/V38496.sh
@@ -2,7 +2,7 @@
 #
 # STIG URL: http://www.stigviewer.com/stig/red_hat_enterprise_linux_6/2014-06-11/finding/V-38496
 # Finding ID:	V-38496
-# Version:	
+# Version:	RHEL-06-000029
 # Finding Level:	Medium
 #
 #     Default operating system accounts, other than root, must be locked. 
