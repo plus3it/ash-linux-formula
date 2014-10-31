673f2bacb1674ca11898a1a1674f6d40f3d1c49b
Thomas H Jones II <thomas.jones2@dodiis.mil>
2014-10-31 10:10:24 -0400

    Handler for STIG-ID V-38696 (OS must detect addtion of unauthorized components)

--- srv/salt/STIGbyID/cat2/files/V38696.sh
+++ srv/salt/STIGbyID/cat2/files/V38696.sh
@@ -2,7 +2,7 @@
 #
 # STIG URL: http://www.stigviewer.com/stig/red_hat_enterprise_linux_6/2014-06-11/finding/V-38696
 # Finding ID:	V-38696
-# Version:	
+# Version:	RHEL-06-000303
 # Finding Level:	Medium
 #
 #     The operating system must employ automated mechanisms, per 
@@ -13,4 +13,15 @@
 #     files.
 #
 ############################################################
+
+diag_out() {
+   echo "${1}"
+}
+
+diag_out "----------------------------------"
+diag_out "STIG Finding ID: V-38696"
+diag_out "  OS must be configured to detect"
+diag_out "  additon of unauthorized"
+diag_out "  components/devices"
+diag_out "----------------------------------"
 
