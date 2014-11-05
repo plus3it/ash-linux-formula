6c657cf8f8f05cd1d5f349e6554139274c88b7dd
Thomas H Jones II <thomas.jones2@dodiis.mil>
2014-11-05 12:24:45 -0500

    Initial checkin of stub cat3 handler files

---
+++ srv/salt/STIGbyID/cat3/files/V38552.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# STIG URL: http://www.stigviewer.com/stig/red_hat_enterprise_linux_6/2014-06-11/finding/V-38552
+# Finding ID:	V-38552
+# Version:	RHEL-06-000188
+# Finding Level:	Low
+#
+#     The audit system must be configured to audit all discretionary access 
+#     control permission modifications using fchown. The changing of file 
+#     permissions could indicate that a user is attempting to gain access 
+#     to information that would otherwise be disallowed. Auditing DAC 
+#     modifications can facilitate the identification of patterns of abuse 
+#     among both authorized and unauthorized users.
+#
+#  CCI: CCI-000172
+#  NIST SP 800-53 :: AU-12 c
+#  NIST SP 800-53A :: AU-12.1 (iv)
+#  NIST SP 800-53 Revision 4 :: AU-12 c
+#
+############################################################
+
+diag_out() {
+   echo "${1}"
+}
+
+diag_out "-----------------------------------"
+diag_out "STIG Finding ID: V-38552"
+diag_out "  Audit system must log all DAC"
+diag_out "  permission modifications using"
+diag_out "  fchown"
+diag_out "-----------------------------------"
