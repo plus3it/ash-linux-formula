8714064b47fa407d949f96d4a54475a47a38af98
Thomas H Jones II <thomas.jones2@dodiis.mil>
2014-11-21 17:52:48 +0000

    Handler for STIG-ID V-38699 (System accounts must own all public directories)

---
+++ srv/salt/STIGbyID/cat3/V38699.sls
@@ -0,0 +1,25 @@
+# STIG URL: http://www.stigviewer.com/stig/red_hat_enterprise_linux_6/2014-06-11/finding/V-38699
+# Finding ID:	V-38699
+# Version:	RHEL-06-000337
+# Finding Level:	Low
+#
+#     Allowing a user account to own a world-writable directory is 
+#     undesirable because it allows the owner of that directory to remove 
+#     or replace any files that may be placed in the directory by other 
+#     users. 
+#
+#  CCI: CCI-000366
+#  NIST SP 800-53 :: CM-6 b
+#  NIST SP 800-53A :: CM-6.1 (iv)
+#  NIST SP 800-53 Revision 4 :: CM-6 b
+#
+############################################################
+
+script_V38699-describe:
+  cmd.script:
+  - source: salt://STIGbyID/cat3/files/V38699.sh
+
+# Probably need to replace this with custom module...
+script_V38699-Verfiy:
+  cmd.script:
+  - source: salt://STIGbyID/cat3/files/V38699-helper.sh
---
+++ srv/salt/STIGbyID/cat3/files/V38699-helper.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#
+# STIG URL: http://www.stigviewer.com/stig/red_hat_enterprise_linux_6/2014-06-11/finding/V-38699
+# Finding ID:	V-38699
+# Version:	RHEL-06-000337
+# Finding Level:	Low
+#
+#     Allowing a user account to own a world-writable directory is 
+#     undesirable because it allows the owner of that directory to remove 
+#     or replace any files that may be placed in the directory by other 
+#     users. 
+#
+#  CCI: CCI-000366
+#  NIST SP 800-53 :: CM-6 b
+#  NIST SP 800-53A :: CM-6.1 (iv)
+#  NIST SP 800-53 Revision 4 :: CM-6 b
+#
+############################################################
+
+FSMOUNTS=`egrep "(ext2|ext3|ext4|xfs)" /etc/fstab | awk '{print $2}'`
+for FS in ${FSMOUNTS}
+do
+   OFFENDERS=`find "${FS}" -xdev -type d -perm -0002 -uid +499 -print`
+   if [ "${OFFENDERS}" = "" ]
+   then
+      echo "All world-writable directories are owned by system accounts"
+      SETEXIT=0
+   else
+      for OFFENDER in ${OFFENDERS}
+      do
+         echo "Found user-owned world-writable directory [${OFFENDER}]"
+         SETEXIT=1
+      done
+   fi
+done
+
+exit ${SETEXIT}
