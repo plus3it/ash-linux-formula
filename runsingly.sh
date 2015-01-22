81922572ca93e17d39b1be2dfceda9787438a27d
Thomas H Jones II <thomas.jones@winsnetworks.com>
2015-01-22 17:04:35 -0500

    Lockdown execution-shim (until aggregated-run precedence-issues can be solved)

---
+++ runsingly.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+#
+# Script to iterate though all of the VID SLS files and run them 
+# singly. This script necessary until Jinja/state precendence 
+# side-effects of unified Salt-run can be overcome
+#
+# NOTE: It is recommended to run this script with a log-capture 
+# invocation method (i.e., "tack on '| tee /log/dir/log.file'")
+#
+#################################################################
+VIDHOME="${1:-/srv/salt}"
+DEBUGLVL="warning"
+SALTCMD="/usr/bin/salt-call --no-color --local  -l ${DEBUGLVL} state.sls"
+
+# Locate all the VID SLS files and create a list
+VIDLIST=`find -L ${VIDHOME} -type f -name "V*.sls" | sort | sed '{
+s/^.*STIGbyID/STIGbyID/
+s/\.sls$//
+}'`
+
+# Iterate individual VIDs from VID-list
+for VID in ${VIDLIST}
+do
+   ${SALTCMD} ${VID}
+done
