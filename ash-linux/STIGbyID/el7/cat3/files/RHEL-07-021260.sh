0c582d0963bd9e05e3c0181895c455dc90478dcc
Thomas H Jones II <github@xanthia.com>
2016-05-06 00:41:02 +0000

    Content for STIG V-ID 'RHEL-07-021260'

--- ash-linux/STIGbyID/el7/cat3/files/RHEL-07-021260.sh
+++ ash-linux/STIGbyID/el7/cat3/files/RHEL-07-021260.sh
@@ -16,3 +16,13 @@
 #    NIST SP 800-53 Revision 4 :: CM-6 b
 #
 #################################################################
+
+diag_out() {
+   echo "${1}"
+}
+
+diag_out "---------------------------------------------"
+diag_out "STIG Finding ID: RHEL-07-021260"
+diag_out "   The system must use a separate filesystem"
+diag_out "   for /var/log/audit."
+diag_out "---------------------------------------------"
