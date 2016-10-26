3dd63265a1d255dff502bc7f0db06cde54bfcd3c
Thomas H. Jones II <github@xanthia.com>
2016-10-26 20:24:14 +0000

    Initial content for 030383

--- ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030383.sh
+++ ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030383.sh
@@ -24,4 +24,6 @@ diag_out() {
 
 diag_out "----------------------------------------"
 diag_out "STIG Finding ID: RHEL-07-030383"
+diag_out "   All uses of the fchownat command"
+diag_out "   must be audited."
 diag_out "----------------------------------------"
