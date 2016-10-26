1d7f38e1d626f188a786d6038b6ebcd8598b9365
Thomas H. Jones II <github@xanthia.com>
2016-10-26 20:52:13 +0000

    Initial content for 030390

--- ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030390.sh
+++ ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030390.sh
@@ -20,4 +20,6 @@ diag_out() {
 
 diag_out "----------------------------------------"
 diag_out "STIG Finding ID: RHEL-07-030390"
+diag_out "   All uses of the chmod command must"
+diag_out "   be audited."
 diag_out "----------------------------------------"
