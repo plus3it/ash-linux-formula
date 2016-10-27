d1a7dd800cfbece62e00bcd8e388019be5007466
Thomas H. Jones II <github@xanthia.com>
2016-10-27 18:21:00 +0000

    Initial content for 030425

--- ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030425.sh
+++ ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030425.sh
@@ -22,4 +22,6 @@ diag_out() {
 
 diag_out "----------------------------------------"
 diag_out "STIG Finding ID: RHEL-07-030425"
+diag_out "   All uses of the ftruncate command"
+diag_out "   must be audited."
 diag_out "----------------------------------------"
