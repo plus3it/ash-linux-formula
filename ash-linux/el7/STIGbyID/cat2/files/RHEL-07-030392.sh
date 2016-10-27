f367b84c08845521d4b6e1b4863425b03ad5ee89
Thomas H. Jones II <github@xanthia.com>
2016-10-27 14:42:40 +0000

    Initial content for 030392

--- ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030392.sh
+++ ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030392.sh
@@ -20,4 +20,6 @@ diag_out() {
 
 diag_out "----------------------------------------"
 diag_out "STIG Finding ID: RHEL-07-030392"
+diag_out "   All uses of the fchmodat command"
+diag_out "   must be audited."
 diag_out "----------------------------------------"
