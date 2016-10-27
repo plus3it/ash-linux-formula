bbe033f1f3049bf78910a47ba59206f41fce95c2
Thomas H. Jones II <github@xanthia.com>
2016-10-27 14:45:18 +0000

    Initial content for 030400

--- ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030400.sh
+++ ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030400.sh
@@ -20,4 +20,6 @@ diag_out() {
 
 diag_out "----------------------------------------"
 diag_out "STIG Finding ID: RHEL-07-030400"
+diag_out "   All uses of the setxattr command"
+diag_out "   must be audited."
 diag_out "----------------------------------------"
