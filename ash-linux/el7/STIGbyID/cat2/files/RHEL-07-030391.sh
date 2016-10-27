35e5b6cfcd658db1a8f79934c87bef0f3ce389fe
Thomas H. Jones II <github@xanthia.com>
2016-10-27 14:34:17 +0000

    Initial content for 030391

--- ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030391.sh
+++ ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030391.sh
@@ -20,4 +20,6 @@ diag_out() {
 
 diag_out "----------------------------------------"
 diag_out "STIG Finding ID: RHEL-07-030391"
+diag_out "   All uses of the fchmod command must"
+diag_out "   be audited."
 diag_out "----------------------------------------"
