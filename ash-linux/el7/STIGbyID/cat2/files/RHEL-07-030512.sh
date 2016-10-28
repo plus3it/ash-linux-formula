2bc15af8e1a716c96586a85dead79cafb662ad8d
Thomas H. Jones II <github@xanthia.com>
2016-10-28 15:41:46 +0000

    Initial content for 030512

--- ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030512.sh
+++ ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030512.sh
@@ -26,4 +26,6 @@ diag_out() {
 
 diag_out "----------------------------------------"
 diag_out "STIG Finding ID: RHEL-07-030512"
+diag_out "   All uses of the gpasswd command must"
+diag_out "   be audited."
 diag_out "----------------------------------------"
