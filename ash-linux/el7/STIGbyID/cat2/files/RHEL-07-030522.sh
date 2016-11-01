a5d098eff9b6843827a6dbf4e349df4e095ca9d1
Thomas H. Jones II <github@xanthia.com>
2016-11-01 12:54:05 +0000

    Initial content for 030522

--- ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030522.sh
+++ ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030522.sh
@@ -30,4 +30,6 @@ diag_out() {
 
 diag_out "----------------------------------------"
 diag_out "STIG Finding ID: RHEL-07-030522"
+diag_out "   All uses of the sudo command must be"
+diag_out "   audited."
 diag_out "----------------------------------------"
