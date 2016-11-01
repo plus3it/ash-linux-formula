1b644426d3503e7595585ec2b15b746a00c9d80d
Thomas H. Jones II <github@xanthia.com>
2016-11-01 21:25:50 +0000

    Initial content for 030752

--- ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030752.sh
+++ ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030752.sh
@@ -22,4 +22,6 @@ diag_out() {
 
 diag_out "----------------------------------------"
 diag_out "STIG Finding ID: RHEL-07-030752"
+diag_out "   All uses of the rmdir command must"
+diag_out "   be audited."
 diag_out "----------------------------------------"
