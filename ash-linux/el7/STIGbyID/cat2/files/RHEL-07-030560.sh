f4d1d9282cb1bc871b6c2e0b1b9b9a6490d1e49d
Thomas H. Jones II <github@xanthia.com>
2016-11-01 15:21:11 +0000

    Initial content for 030560

--- ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030560.sh
+++ ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030560.sh
@@ -26,4 +26,6 @@ diag_out() {
 
 diag_out "----------------------------------------"
 diag_out "STIG Finding ID: RHEL-07-030560"
+diag_out "   All uses of the pt_chown command"
+diag_out "   must be audited."
 diag_out "----------------------------------------"
