6895198c4c2233690b10bae66f1c66d23de1f908
Thomas H. Jones II <github@xanthia.com>
2016-11-01 18:43:16 +0000

    Initial content for 030670

--- ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030671.sh
+++ ash-linux/el7/STIGbyID/cat2/files/RHEL-07-030671.sh
@@ -20,4 +20,6 @@ diag_out() {
 
 diag_out "----------------------------------------"
 diag_out "STIG Finding ID: RHEL-07-030671"
+diag_out "   All uses of the delete_module"
+diag_out "   command must be audited."
 diag_out "----------------------------------------"
