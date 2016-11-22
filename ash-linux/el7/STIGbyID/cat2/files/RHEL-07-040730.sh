81807fd0ff13c58b6606d069776093828819d7e2
Thomas H. Jones II <github@xanthia.com>
2016-11-22 22:49:10 +0000

    Initial content for 040730

--- ash-linux/el7/STIGbyID/cat2/files/RHEL-07-040730.sh
+++ ash-linux/el7/STIGbyID/cat2/files/RHEL-07-040730.sh
@@ -21,4 +21,7 @@ diag_out() {
 
 diag_out "----------------------------------------"
 diag_out "STIG Finding ID: RHEL-07-040730"
+diag_out "   The system must not be performing"
+diag_out "   packet forwarding unless the system"
+diag_out "   is a router."
 diag_out "----------------------------------------"
