001ffcfeff5c9995cf9c59d350bc73c4523be064
Thomas H. Jones II <github@xanthia.com>
2017-02-09 20:04:38 +0000

    Begin process of figuring out how to set mount-opts on systemd-managed /tmp fs

---
+++ ash-linux/el7/VendorSTIG/cat3/files/mount_option_tmp_nodev.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Finding ID:	
+# Version:	mount_option_tmp_nodev
+# SRG ID:	
+# Finding Level:	low
+#
+# Rule Summary:
+#       The nodev mount option can be used to prevent device
+#       files from being created in /tmp. Legitimate character
+#       and block devices should not exist within temporary
+#       directories like /tmp. Add the nodev option to the
+#       fourth column of /etc/fstab for the line which controls
+#       mounting of /tmp.
+#
+# CCI-xxxxxx CCI-xxxxxx
+#    NIST SP 800-53 Revision 4 :: CM-7
+#    NIST SP 800-53 Revision 4 :: MP-2
+#    CIS RHEL 7 Benchmark 1.1.0 :: 1.1.2
+#
+#################################################################
+{%- set stig_id = 'mount_option_tmp_nodev' %}
+{%- set helperLoc = 'ash-linux/el7/VendorSTIG/cat3/files' %}
+
+script_{{ stig_id }}-describe:
+  cmd.script:
+    - source: salt://{{ helperLoc }}/{{ stig_id }}.sh
+    - cwd: /root
+
+
+# Standard outputter function
+diag_out() {
+   echo "${1}"
+}
+
+diag_out "----------------------------------------"
+diag_out "STIG Finding ID: mount_option_tmp_nodev"
+diag_out "   The only legitimate location for"
+diag_out "   device files is the /dev directory"
+diag_out "   located on the root partition."
+diag_out "----------------------------------------"
