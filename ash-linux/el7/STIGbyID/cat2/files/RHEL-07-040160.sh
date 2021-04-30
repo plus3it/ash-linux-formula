8bc06b8c815a56e72a7f256d231d2bef66ed477c
Thomas H Jones II <github@xanthia.com>
2021-04-30 09:24:21 -0400

    Get rid of trailing-whitespace

--- ash-linux/el7/STIGbyID/cat2/files/RHEL-07-040160.sh
+++ ash-linux/el7/STIGbyID/cat2/files/RHEL-07-040160.sh
@@ -3,7 +3,7 @@
 # Version:	RHEL-07-040160_rule
 # SRG ID:	SRG-OS-000163-GPOS-00072
 # Finding Level:	medium
-# 
+#
 # Rule Summary:
 #	All network connections associated with a communication
 #	session must be terminated at the end of the session or after
@@ -11,12 +11,12 @@
 #	except to fulfill documented and validated mission
 #	requirements.
 #
-# CCI-001133 
-# CCI-002361 
-#    NIST SP 800-53 :: SC-10 
-#    NIST SP 800-53A :: SC-10.1 (ii) 
-#    NIST SP 800-53 Revision 4 :: SC-10 
-#    NIST SP 800-53 Revision 4 :: AC-12 
+# CCI-001133
+# CCI-002361
+#    NIST SP 800-53 :: SC-10
+#    NIST SP 800-53A :: SC-10.1 (ii)
+#    NIST SP 800-53 Revision 4 :: SC-10
+#    NIST SP 800-53 Revision 4 :: AC-12
 #
 #################################################################
 # Standard outputter function
