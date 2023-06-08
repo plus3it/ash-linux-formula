ddb09c09946636c23b3cf262e976a4ad8c707dc6
Thomas H Jones II <github@xanthia.com>
2023-06-08 14:50:34 -0400

    Apply shellcheck-recommended fixes:
    - Change interp-line so that '[[ ]]' test-method is available
    - Ensure all variables are quoted
    - Get rid of unused (local) variable 'SCRIPTEXIT'

--- ash-linux/el7/Miscellaneous/files/firewalld_safeties.sh
+++ ash-linux/el7/Miscellaneous/files/firewalld_safeties.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 # shellcheck disable=
 #
 # Script to implement firewalld 'safety' rules referenced by th
 # firewalld_safeties state
 #
 #################################################################
-PROGNAME="$(basename ${0})"
+PROGNAME="$( basename "${0}" )"
 DEBUG="${DEBUG:-UNDEF}"
-SELMODE="$(getenforce)"
+SELMODE="$( getenforce )"
 ERRCTR=0
 FWCMD="firewall-cmd"
 FWCMDARGS="-q --direct --add-rule ipv4 filter INPUT_direct"
@@ -15,7 +15,6 @@ FWCMDARGS="-q --direct --add-rule ipv4 filter INPUT_direct"
 # Error handler function
 function err_exit {
    local ERRSTR="${1}"
-   local SCRIPTEXIT=${2:-1}
 
    ERRCTR=$((ERRCTR+=1))
 
@@ -38,25 +37,25 @@ then
 fi
 
 # Running-rules
-${FWCMD} ${FWCMDARGS} 10 -m state --state RELATED,ESTABLISHED -m comment \
+"${FWCMD}" "${FWCMDARGS}" 10 -m state --state RELATED,ESTABLISHED -m comment \
   --comment 'Allow related and established connections' -j ACCEPT || \
   err_exit 'Failed to add RELATED/ESTABLISHED exception to running config'
-${FWCMD} ${FWCMDARGS} 20 -i lo -j ACCEPT || \
+"${FWCMD}" "${FWCMDARGS}" 20 -i lo -j ACCEPT || \
   err_exit 'Failed to add loopback exception to running config'
-${FWCMD} ${FWCMDARGS} 30 -d 127.0.0.0/8 '!' -i lo -j DROP || \
+"${FWCMD}" "${FWCMDARGS}" 30 -d 127.0.0.0/8 '!' -i lo -j DROP || \
   err_exit 'Failed to add loopback-spoofing to running config'
-${FWCMD} ${FWCMDARGS} 50 -p tcp -m tcp --dport 22 -j ACCEPT || \
+"${FWCMD}" "${FWCMDARGS}" 50 -p tcp -m tcp --dport 22 -j ACCEPT || \
   err_exit 'Failed to protect SSH access in running config'
 
 # Permanent-rules
-${FWCMD} --permanent ${FWCMDARGS} 10 -m state --state RELATED,ESTABLISHED -m comment \
+"${FWCMD}" --permanent "${FWCMDARGS}" 10 -m state --state RELATED,ESTABLISHED -m comment \
   --comment 'Allow related and established connections' -j ACCEPT || \
   err_exit 'Failed to add RELATED/ESTABLISHED exception to permanent config'
-${FWCMD} --permanent ${FWCMDARGS} 20 -i lo -j ACCEPT || \
+"${FWCMD}" --permanent "${FWCMDARGS}" 20 -i lo -j ACCEPT || \
   err_exit 'Failed to add loopback exception to permanent config'
-${FWCMD} --permanent ${FWCMDARGS} 30 -d 127.0.0.0/8 '!' -i lo -j DROP || \
+"${FWCMD}" --permanent "${FWCMDARGS}" 30 -d 127.0.0.0/8 '!' -i lo -j DROP || \
   err_exit 'Failed to add loopback-spoofing to permanent config'
-${FWCMD} --permanent ${FWCMDARGS} 50 -p tcp -m tcp --dport 22 -j ACCEPT || \
+"${FWCMD}" --permanent "${FWCMDARGS}" 50 -p tcp -m tcp --dport 22 -j ACCEPT || \
   err_exit 'Failed to protect SSH access in permanent config'
 
 # Set SEL back to original mode (may be redundant)
